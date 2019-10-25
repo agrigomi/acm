ec6e31d49e81ed2d04f26bef958638ccd17271ca
Mihail Grigorov <megrigorov@gmail.com>
2019-10-25 14:09:54 +0300

    db: interface & code update
    Signed-off-by: Mihail Grigorov <megrigorov@gmail.com>

--- libams/db.cpp
+++ libams/db.cpp
@@ -6,6 +6,8 @@
 bool _dbc_t::connect(_cstr_t db_host, _cstr_t db_port, _cstr_t db_user, _cstr_t db_pass, _cstr_t db_name) {
 	bool r = false;
 
+	mp_pgres = NULL;
+
 	if(!mp_pgc) {
 		if((mp_pgc = PQsetdbLogin(db_host, db_port, NULL, NULL, db_name, db_user, db_pass)))
 			r = true;
@@ -16,6 +18,10 @@ bool _dbc_t::connect(_cstr_t db_host, _cstr_t db_port, _cstr_t db_user, _cstr_t 
 }
 
 void _dbc_t::disconnect(void) {
+	if(mp_pgres) {
+		PQclear(mp_pgres);
+		mp_pgres = NULL;
+	}
 	if(mp_pgc) {
 		PQfinish(mp_pgc);
 		mp_pgc = NULL;
@@ -39,6 +45,77 @@ _cstr_t _dbc_t::error_text(void) {
 
 	return r;
 }
+
+_cstr_t _dbc_t::result_error_text(void) {
+	_cstr_t r = "Unknown error";
+
+	if(mp_pgres)
+		r = PQresultErrorMessage(mp_pgres);
+
+	return r;
+}
+
+void _dbc_t::clear(void) {
+	if(mp_pgres) {
+		PQclear(mp_pgres);
+		mp_pgres = NULL;
+	}
+}
+
+bool _dbc_t::exec(_cstr_t query) {
+	bool r = false;
+
+	if(mp_pgc) {
+		ExecStatusType res;
+
+		clear();
+		mp_pgres = PQexec(mp_pgc, query);
+		res = PQresultStatus(mp_pgres);
+
+		if(res == PGRES_COMMAND_OK || res == PGRES_TUPLES_OK)
+			r = true;
+	}
+
+	return r;
+}
+
+_u32 _dbc_t::fields(void) {
+	_u32 r = 0;
+
+	if(mp_pgres)
+		r = PQnfields(mp_pgres);
+
+	return r;
+}
+
+_u32 _dbc_t::rows(void) {
+	_u32 r = 0;
+
+	if(mp_pgres)
+		r = PQntuples(mp_pgres);
+
+	return r;
+}
+
+_cstr_t _dbc_t::value(_u32 row, _u32 field) {
+	_cstr_t r = NULL;
+
+	if(mp_pgres)
+		r = PQgetvalue(mp_pgres, row, field);
+
+	return r;
+}
+
+_u32 _dbc_t::length(_u32 row, _u32 field) {
+	_u32 r = 0;
+
+	if(mp_pgres)
+		r = PQgetlength(mp_pgres, row, field);
+
+	return r;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 dbc_incubator::dbc_incubator() {
 	mpi_pool = NULL;
@@ -72,6 +149,7 @@ void dbc_incubator::init(_cstr_t db_host, _cstr_t db_port, _cstr_t db_user, _cst
 								p_dbci->m_db_name);
 						break;
 					case POOL_OP_FREE:
+						p_dbc->clear();
 						break;
 					case POOL_OP_DELETE:
 						p_dbc->disconnect();
--- libams/private.h
+++ libams/private.h
@@ -9,12 +9,19 @@ typedef struct dbc_incubator	_dbc_incubator_t;
 typedef struct {
 private:
 	PGconn *mp_pgc;
-
+	PGresult *mp_pgres;
 public:
 	bool connect(_cstr_t db_host, _cstr_t db_port, _cstr_t db_user, _cstr_t db_pass, _cstr_t db_name);
 	void disconnect(void);
 	ConnStatusType status(void);
 	_cstr_t error_text(void);
+	_cstr_t result_error_text(void);
+	void clear(void);
+	bool exec(_cstr_t query);
+	_u32 fields(void);
+	_u32 rows(void);
+	_cstr_t value(_u32 row, _u32 field);
+	_u32 length(_u32 row, _u32 field);
 }_dbc_t;
 
 struct dbc_incubator {
